4bb2e1845d6b5210e369bf8fd3112a381c19cd76
LoremIpsum <dardanber95@gmail.com>
2025-12-06 11:33:24 +0100

    Fix taker entrypoint to asyncio.run(main)

--- strategies/taker/run_taker.py
+++ strategies/taker/run_taker.py
@@ -6,9 +6,10 @@ ROOT = Path(__file__).resolve().parents[2]
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
 
+import asyncio
 from main_hopper import main
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
 
